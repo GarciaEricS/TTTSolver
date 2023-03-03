186e4c3e08fe14ba0851cafeb8b1b43534339145
Eric Garcia <garciaericsteven@gmail.com>
2023-03-02 17:57:44 -0800

    Changed stuff

--- homefun-04/TTT.h
+++ homefun-04/TTT.h
@@ -5,17 +5,16 @@
 #include "solver.h"
 
 namespace TTT {
-	enum class Tile {X, O, B};
 	enum class Type {Regular, OnlyX, OnC};
 	class TTTPosition : public Solver::Position {
 		public:
 			TTTPosition(int m, int n, int k, Type type, bool misere);
 			Position *doMove(int move);
-			std::vector<int> *generateMoves();
+			std::vector<std::pair<int, Solver::Tile>> *generateMoves();
 			Solver::Primitive primitiveValue();
 			int hash(bool removeSymmetries);
             int canonicalHash();
-			Tile whoseMove;
+			Solver::Tile whoseMove;
 
 		protected:
 			bool line_at(int x, int y, int z);
@@ -24,19 +23,8 @@ namespace TTT {
 			int k;
 			Type type;
 			bool misere;
-			std::vector<Tile> tiles;
-            Tile getAt(int i, int j);
-            void setAt(Tile tile, int i, int j);
-	};
-}
-
-namespace TTZ {
-	class TTZPosition : public Solver::Position {
-		public:
-			Position *doMove(int move);
-			std::vector<int> *generateMoves();
-			Solver::Primitive primitiveValue();
-			int hash(bool removeSymmetries);
-			int currInt;
+			std::vector<Solver::Tile> tiles;
+            Solver::Tile getAt(int i, int j);
+            void setAt(Solver::Tile tile, int i, int j);
 	};
 }
--- homefun-04/solver.h
+++ homefun-04/solver.h
@@ -5,10 +5,11 @@
 namespace Solver {
 	enum class Result {WIN, TIE, LOSE, DRAW, NIL};
 	enum class Primitive {WIN, TIE, LOSE, NOT_PRIMITIVE};
+	enum class Tile {X, O, B};
 	class Position {
 		public:
 			virtual Position *doMove(int move) = 0;
-			virtual std::vector<int> *generateMoves() = 0;
+			virtual std::vector<std::pair<int, Tile>> *generateMoves() = 0;
 			virtual Primitive primitiveValue() = 0;
 			virtual int hash(bool removeSymmetries) = 0;
 			virtual ~Position() {};
