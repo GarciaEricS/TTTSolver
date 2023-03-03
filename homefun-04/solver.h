93385fc0aeda1ffe8a00079b574eb0cde5459fa9
Eric Garcia <garciaericsteven@gmail.com>
2023-03-02 17:34:03 -0800

    created directory for hf4

---
+++ homefun-04/solver.h
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+namespace Solver {
+	enum class Result {WIN, TIE, LOSE, DRAW, NIL};
+	enum class Primitive {WIN, TIE, LOSE, NOT_PRIMITIVE};
+	class Position {
+		public:
+			virtual Position *doMove(int move) = 0;
+			virtual std::vector<int> *generateMoves() = 0;
+			virtual Primitive primitiveValue() = 0;
+			virtual int hash(bool removeSymmetries) = 0;
+			virtual ~Position() {};
+	};
+}
+
