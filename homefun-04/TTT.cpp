722ea788e17c9370d7ed5af919605596831ee69b
Eric Garcia <garciaericsteven@gmail.com>
2023-03-02 17:59:36 -0800

    changed stuff again

--- homefun-04/TTT.cpp
+++ homefun-04/TTT.cpp
@@ -14,24 +14,24 @@ namespace TTT {
 		this->n = n;
 		this->k = k;
 		for (int i = 0; i < m * n; i++) {
-			this->tiles.push_back(Tile::B);
+			this->tiles.push_back(Solver::Tile::B);
 		};
 	}
 
-    Tile TTTPosition::getAt(int i, int j) {
+    Solver::Tile TTTPosition::getAt(int i, int j) {
         return tiles[i * n + j];
     }
 
-    void TTTPosition::setAt(Tile tile, int i, int j) {
+    void TTTPosition::setAt(Solver::Tile tile, int i, int j) {
         tiles[i * n + j] = tile;
     }
 
-	std::vector<int> *TTTPosition::generateMoves() {
-		std::vector<int>* moves = new std::vector<int>();
+std::vector<std::pair<int, Solver::Tile>> *TTTPosition::generateMoves() {
+		std::vector<int>* moves = new std::vector<std::pair<int, Solver::Tile>>();
 		auto tiles = this->tiles;
 
 		for (int i = 0; i < m * n; i++) {
-			if (tiles[i] == Tile::B) {
+			if (tiles[i] == Solver::Tile::B) {
 				moves->push_back(i);
 			}
 		}
@@ -39,7 +39,7 @@ namespace TTT {
 	}
 
 	Solver::Primitive TTTPosition::primitiveValue() {
-		auto piece = (whoseMove == Tile::X) ? Tile::O : Tile::X;
+		auto piece = (whoseMove == Solver::Tile::X) ? Solver::Tile::O : Solver::Tile::X;
 		int inARow;
 		for (int i = 0; i < m; i++) {
 			inARow = 0;
@@ -138,7 +138,7 @@ namespace TTT {
 		}
 
 		for (int i = 0; i < m * n; i++) {
-			if (tiles[i] == Tile::B) {
+			if (tiles[i] == Solver::Tile::B) {
 				return Solver::Primitive::NOT_PRIMITIVE;
 			}
 		}
@@ -148,26 +148,26 @@ namespace TTT {
 	// Creates a new_position pointer which must later be freed
 	Solver::Position *TTTPosition::doMove(int move) {
 		auto tls = this->tiles;
-		Tile whose_move = this->whoseMove;
+		Solver::Tile whose_move = this->whoseMove;
 		tls[move] = whose_move;
 		TTTPosition *new_position = new TTTPosition(m, n, k);
 		new_position->tiles = tls;
 		new_position->whoseMove = 
-			whose_move == Tile::X ? Tile::O : Tile::X;
+			whose_move == Solver::Tile::X ? Solver::Tile::O : Solver::Tile::X;
 		return new_position;
 	}
 
-    int hashPos(std::vector<Tile> tiles, int m, int n) {
+    int hashPos(std::vector<Solver::Tile> tiles, int m, int n) {
         int sum = 0;
         for (int i = 0; i < m * n; i++) {
             switch (tiles[i]) {
-                case (Tile::X):
+                case (Solver::Tile::X):
                     sum += 0;
                     break;
-                case (Tile::O):
+                case (Solver::Tile::O):
                     sum += 1;
                     break;
-                case (Tile::B):
+                case (Solver::Tile::B):
                     sum += 2;
                     break;
             }
@@ -176,8 +176,8 @@ namespace TTT {
         return sum;
     }
 
-    std::vector<Tile> rotateBoard(std::vector<Tile> tiles, int m, int n) {
-        std::vector<Tile> rotated;
+    std::vector<Solver::Tile> rotateBoard(std::vector<Solver::Tile> tiles, int m, int n) {
+        std::vector<Solver::Tile> rotated;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 rotated.push_back(tiles[j * n + n - 1 - i]);
@@ -186,10 +186,10 @@ namespace TTT {
         return rotated;
     }
 
-    int minReflectionHash(std::vector<Tile> tiles, int m, int n) {
+    int minReflectionHash(std::vector<Solver::Tile> tiles, int m, int n) {
         int hashNoReflect = hashPos(tiles, m, n);
 
-        std::vector<Tile> reflected;
+        std::vector<Solver::Tile> reflected;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 reflected.push_back(tiles[(m - 1 - i) * n + j]);
@@ -220,7 +220,7 @@ namespace TTT {
 	int TTTPosition::canonicalHash() {
         int minHash = minReflectionHash(tiles, m, n);
         if (m == n) {
-            std::vector<Tile> rotated = rotateBoard(tiles, m, n);
+            std::vector<Solver::Tile> rotated = rotateBoard(tiles, m, n);
             int minRotatedHash = minReflectionHash(rotated, m, n);
             minHash = std::min(minHash, minRotatedHash);
         }
