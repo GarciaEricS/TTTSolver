e088c7542cb2d8bf652b41d88c9205eb0e892332
Eric Garcia <garciaericsteven@gmail.com>
2023-03-10 17:52:31 -0800

    Moved solve to header file, deleted solve, and updated makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include "../include/TTT.h"
-#include <vector>
-#include <unordered_map>
-#include <utility>
-#include <bits/stdc++.h>
-#include <algorithm>
+#include "../include/solver.h"
 #include <string>
+#include <utility>
 
 struct Counts {
 	int wins;
@@ -13,80 +10,6 @@ struct Counts {
 	int draws;
 	int ties;
 };
-
-Solver::Result prim_to_result(Solver::Primitive prim) {
-	Solver::Result result;
-	switch (prim) {
-		case (Solver::Primitive::WIN):
-			result = Solver::Result::WIN;
-			break;
-		case (Solver::Primitive::LOSE):
-			result = Solver::Result::LOSE;
-			break;
-		case (Solver::Primitive::TIE):
-			result = Solver::Result::TIE;
-			break;
-		case (Solver::Primitive::NOT_PRIMITIVE):
-			result = Solver::Result::NIL;
-			break;
-	}
-	return result;
-}
-
-template <class Move>
-std::pair<Solver::Result, int> solve(Solver::Position<Move> *position, 
-		bool removeSymmetries,
-		std::unordered_map<int, std::pair<Solver::Result, int>> *memo) {
-
-	int canonicalHash = position->hash(removeSymmetries);
-	if (memo->find(canonicalHash) != memo->end()) {
-		return memo->at(canonicalHash);
-	}
-
-	auto prim = position->primitiveValue();
-	auto result = prim_to_result(prim);
-	if (result != Solver::Result::NIL) {
-		memo->insert({canonicalHash, {result, 0}});
-		return {result, 0};
-	}
-
-	auto moves = position->generateMoves();
-	bool win_found = false;	
-	bool tie_found = false;
-	int win_remote = INT_MAX;
-	int tie_remote = INT_MAX;
-	int lose_remote = -1;
-	for (auto move : *moves) {
-		auto new_position = position->doMove(move);
-		auto result_and_remote = solve(new_position, removeSymmetries, memo); 
-		result = result_and_remote.first;
-		auto remote = result_and_remote.second + 1;
-		delete new_position;
-		if (result == Solver::Result::TIE) {
-			tie_found = true;
-			tie_remote = std::min(tie_remote, remote);
-		} else if (result == Solver::Result::LOSE) {
-			win_found = true;
-			win_remote = std::min(win_remote, remote);
-		} else if (result == Solver::Result::WIN) {
-			lose_remote = std::max(lose_remote, remote);
-		} 
-	}
-	delete moves;
-	int remoteness;
-	if (win_found) {
-		result = Solver::Result::WIN;
-		remoteness = win_remote;
-	} else if (tie_found) {
-		result = Solver::Result::TIE;
-		remoteness = tie_remote;
-	} else {
-		remoteness = lose_remote;
-		result = Solver::Result::LOSE;
-	}
-	memo->insert({canonicalHash, {result, remoteness}});
-	return {result, remoteness};
-}
 
 void print_memo(std::unordered_map<int, std::pair<Solver::Result, int>> *memo, int m, int n, int k, bool removeSymmetries, int typeInt, bool misere) {
 	std::vector<Counts> remote_count;
@@ -185,7 +108,7 @@ int main(int argc, char *argv[]) {
 	TTT::TTTPosition position(m, n, k, type, misere);
 	TTT::TTTPosition *position_ptr = &position;
 	std::unordered_map<int, std::pair<Solver::Result, int>> memo;
-	solve(position_ptr, removeSymmetries, &memo);
+	Solver::solve(position_ptr, removeSymmetries, &memo);
 	print_memo(&memo, m, n, k, removeSymmetries, typeInt, misere);	
 	return 0;
 }
